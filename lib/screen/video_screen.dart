40493e7119420f6438b7110e850c98c20376a337
Mann <meruliyamann26100@gmail.com>
2023-08-22 22:12:24 +0530

    Initial commit

---
+++ lib/screen/video_screen.dart
@@ -0,0 +1,36 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/video_provider.dart';
+
+class videoscreen extends StatefulWidget {
+  const videoscreen({Key? key}) : super(key: key);
+
+  @override
+  State<videoscreen> createState() => _videoscreenState();
+}
+
+class _videoscreenState extends State<videoscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<VideoProvider>(
+      builder: (context, provider, child) => Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Video Player"),
+        ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Chewie(
+                controller: provider.chewieController,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
